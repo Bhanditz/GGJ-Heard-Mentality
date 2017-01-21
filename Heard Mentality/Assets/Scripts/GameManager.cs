922047ad6f2a2810af8232831f1e8ba5e275a755
sschroeder256 <stephen.schroeder256@gmail.com>
2017-01-21 09:21:50 -0500

    + Added Tiled loading architecture + Added proper camera start

--- Heard Mentality/Assets/Scripts/GameManager.cs
+++ Heard Mentality/Assets/Scripts/GameManager.cs
@@ -1,9 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
+using Tiled2Unity;
 
 public class GameManager : MonoBehaviour {
 
     private string startLevel = "test";
+    public static TiledMap map = null;
+
+    void Awake()
+    {
+        /*Cursor.lockState = CursorLockMode.Confined; // keep confined in the game window
+        Cursor.lockState = CursorLockMode.Locked;   // keep confined to center of screen
+        Cursor.lockState = CursorLockMode.None;     // set to default default*/
+    }
 
 	// Use this for initialization
 	void Start () {
@@ -17,7 +26,7 @@ public class GameManager : MonoBehaviour {
 
     private void LoadLevel(string filename)
     {
-
         GameObject obj = (GameObject)GameObject.Instantiate(Resources.Load("Prefabs/" + filename));
+        map = obj.GetComponent<TiledMap>();
     }
 }
