6bf51951fa421662a823d7b554cd70f924cd787d
sschroeder256 <stephen.schroeder256@gmail.com>
2017-01-20 20:55:35 -0500

    + Added some meta files

---
+++ Heard Mentality/Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    private string startLevel = "test";
+
+	// Use this for initialization
+	void Start () {
+        LoadLevel(startLevel);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    private void LoadLevel(string filename)
+    {
+
+        GameObject obj = (GameObject)GameObject.Instantiate(Resources.Load("Prefabs/" + filename));
+    }
+}
